1df99bef8119593c287415f77e042ffea91682a7
daniel.amaral <amaral94daniel@gmail.com>
2020-11-07 17:52:47 +0000

    GET logic implemented hooray

--- middleware.js
+++ middleware.js
@@ -1,6 +1,66 @@
 var dbOperations = require("./db_operations");
 
-var selectItemsFromDB = () => {
+var selectItemsFromDB = (searchWord) => {
+  let relations = [];
+
+  dbOperations.getDaughterByName(searchWord, (daughterOrganizations) => {
+    daughterOrganizations.forEach((daughter, index) => {
+      let parent_id = daughter.parent_id;
+
+      dbOperations.getDaughterRowsByParentId(
+        parent_id,
+        searchWord,
+        (sisterRows) => {
+          sisterRows.forEach((sister) => {
+            if (!checkIfOrgNameAlreadyExists(relations, sister)) {
+              relations.push({
+                relationship_type: "sister",
+                org_name: sister.org_name,
+              });
+            }
+          });
+
+          dbOperations.getOrganizationByName(searchWord, (organization) => {
+            dbOperations.getDaughterRowsByParentId(
+              organization.id,
+              searchWord,
+              (daughterRows) => {
+                daughterRows.forEach((daughter) => {
+                  if (!checkIfOrgNameAlreadyExists(relations, daughter)) {
+                    relations.push({
+                      relationship_type: "daughter",
+                      org_name: daughter.org_name,
+                    });
+                  }
+                });
+
+                dbOperations.getOrganizationByID(
+                  parent_id,
+                  (parentOrganization) => {
+                    parentOrganization.forEach((parent) => {
+                      if (!checkIfOrgNameAlreadyExists(relations, parent)) {
+                        relations.push({
+                          relationship_type: "parent",
+                          org_name: parent.org_name,
+                        });
+                      }
+                    });
+
+                    if (index == daughterOrganizations.length - 1) {
+                      orderArrayByName(relations);
+                      console.log(relations);
+                      return relations;
+                    }
+                  }
+                );
+              }
+            );
+          });
+        }
+      );
+    });
+  });
+
   return "selectItemsFromDB";
 };
 
@@ -17,27 +77,35 @@ var insertItemsIntoDB = (body) => {
       var daughterName = singleDaughter.org_name;
       if (Object.prototype.hasOwnProperty.call(singleDaughter, "daughters")) {
         dbOperations.getOrganizationByName(org_name, (parent) => {
-          
-          dbOperations.insertDaughterIntoDB(
-            singleDaughter.org_name,
-            parent.id
-          );
+          dbOperations.insertDaughterIntoDB(singleDaughter.org_name, parent.id);
           insertItemsIntoDB(singleDaughter);
         });
       } else {
         dbOperations.insertOrganizationIntoDB(daughterName);
         dbOperations.getOrganizationByName(org_name, (parent) => {
-          
-            dbOperations.insertDaughterIntoDB(
-              singleDaughter.org_name,
-              parent.id
-            );
-          });
+          dbOperations.insertDaughterIntoDB(singleDaughter.org_name, parent.id);
+        });
       }
     });
   }
 
   return "InsertItemsIntoDB";
+};
+
+var orderArrayByName = (array) => {
+  array.sort((a, b) => {
+    if (a.org_name < b.org_name) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+};
+
+var checkIfOrgNameAlreadyExists = (relations, possibleElement) => {
+  return relations.some(
+    (element) => element.org_name == possibleElement.org_name
+  );
 };
 
 module.exports = {
