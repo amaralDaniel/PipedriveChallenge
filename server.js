7b685730434ebe427f456109fbd8feb30c4fd51a
daniel.amaral <amaral94daniel@gmail.com>
2020-11-07 17:52:59 +0000

    GET endpoint setup

--- server.js
+++ server.js
@@ -1,39 +1,42 @@
 /* eslint-env node */
-const http = require('http');
-const url = require('url');
-const middleware = require('./middleware');
+const http = require("http");
+const url = require("url");
+const middleware = require("./middleware");
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    
-    const {pathname} = url.parse(req.url);
-    if (pathname == '/' && req.method == 'GET') {
-        res.end(middleware.selectItemsFromDB());
-    }
+  const { pathname } = url.parse(req.url);
+  console.log(pathname);
+  if (pathname == "/" && req.method == "GET") {
+    let searchWord = url.parse(req.url, true).query.search;
+    console.log(searchWord);
+    res.end(middleware.selectItemsFromDB(searchWord));
+  }
 
-    if (pathname == '/' && req.method == 'POST') {
-        let body = [];
-        //building the body
-        req.on('data', (chunk) => {
-            body.push(chunk);
-            }).on('end', () => {    
-            body = Buffer.concat(body).toString();
-            //post body \n formatting 
-            body = JSON.parse(body);
-            res.end(middleware.insertItemsIntoDB(body));
-        });
-    } else {
-        handleError(404, res);
-    }
-
+  if (pathname == "/" && req.method == "POST") {
+    let body = [];
+    //building the body
+    req
+      .on("data", (chunk) => {
+        body.push(chunk);
+      })
+      .on("end", () => {
+        body = Buffer.concat(body).toString();
+        //post body \n formatting
+        body = JSON.parse(body);
+        res.end(middleware.insertItemsIntoDB(body));
+      });
+  } else {
+    handleError(404, res);
+  }
 });
 
 var handleError = (statusCode, res) => {
-    res.statusCode = statusCode;
-    res.end(`{"error": ${http.STATUS_CODES[statusCode]}}`)
-}
+  res.statusCode = statusCode;
+  res.end(`{"error": ${http.STATUS_CODES[statusCode]}}`);
+};
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
